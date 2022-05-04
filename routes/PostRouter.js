c4ed4de030e1d3af41e2cdd20cf63332fd7c1c2c
Akbar Sha S <iamakbarsha1@gmail.com>
2022-05-05 01:53:09 +0530

    5.5.22

--- routes/PostRouter.js
+++ routes/PostRouter.js
@@ -127,19 +127,4 @@ PostRouter.post("/sendEmail", (req, res) => {
   });
 });
 
-PostRouter.post("/sendEmailPortfolio", (req, res) => {
-  console.log(req.body);
-  const { name, email, msg, phone } = req.body;
-  sendEmail(name, email, msg, phone, (err, data) => {
-    if (err) {
-      res.status(500).json({
-        message: "Something went Wrong @POST/sendEmailPortfolio",
-        key: "Error",
-      });
-    } else {
-      res.json({ message: "Email Sent!" });
-    }
-  });
-});
-
 module.exports = PostRouter;
